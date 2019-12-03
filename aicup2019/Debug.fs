e1d5a24c391cb36b8259fea0059946d363c994eb
Nikita Smelov <Arukim@users.noreply.github.com>
2019-12-03 15:27:38 +0300

    ignore debug write in release

--- aicup2019/Debug.fs
+++ aicup2019/Debug.fs
@@ -1,7 +1,12 @@
 ﻿namespace AiCup2019
 
 type Debug(writer) =
-    member this.draw(customData) =
+    member inline this.draw(customData) =
+#if DEBUG
         let message : Model.PlayerMessageGameCustomDataMessage = {Data = customData}
         message.writeTo writer
         writer.Flush()
+#else
+        ignore()
+#endif
+
