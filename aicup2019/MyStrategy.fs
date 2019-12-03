5af992f23cb00bf9be342c93b6d90a3fb3440a5c
Nikita Smelov <Arukim@users.noreply.github.com>
2019-12-03 12:45:30 +0300

    added simple healthpack support
    closes #5

--- aicup2019/MyStrategy.fs
+++ aicup2019/MyStrategy.fs
@@ -17,18 +17,26 @@ type MyStrategy() =
                                                                         | _ -> None)
                                             |> Array.sortBy(fun p -> MyStrategy.DistanceSqr(p, unit.Position))
                                             |> Seq.tryFind(fun _ -> true)
+        
+        let nearestHealthPack = game.LootBoxes |> Array.choose(fun b -> match b.Item with
+                                                                        | Item.HealthPack _ -> Some b.Position
+                                                                        | _ -> None)
+                                                |> Array.sortBy(fun p -> MyStrategy.DistanceSqr(p, unit.Position))
+                                                |> Seq.tryFind(fun _ -> true)
 
-        Tracing.buildTraceMap game (single (unit.Position.X),single (unit.Position.Y))
-            |> Seq.iter(fun cell -> debug.draw(CustomData.Rect {
-                                                    Pos = {X = (single cell.X) + 0.5f; Y = (single cell.Y) + 0.5f}
-                                                    Size = {X = 0.1f; Y = 0.1f}
-                                                    Color = {R = 0.0f; G = 200.0f; B = 0.0f; A = 255.0f}
-                                                    }))
+        //Tracing.buildTraceMap game (single (unit.Position.X),single (unit.Position.Y))
+        //    |> Seq.iter(fun cell -> debug.draw(CustomData.Rect {
+        //                                            Pos = {X = (single cell.X) + 0.5f; Y = (single cell.Y) + 0.5f}
+        //                                            Size = {X = 0.1f; Y = 0.1f}
+        //                                            Color = {R = 0.0f; G = 200.0f; B = 0.0f; A = 255.0f}
+        //                                            }))
 
         let mutable targetPos = unit.Position
 
         if not unit.Weapon.IsSome && nearestWeapon.IsSome then
             targetPos <- nearestWeapon.Value
+        else if unit.Health < 66 && nearestHealthPack.IsSome then
+            targetPos <- nearestHealthPack.Value
         else if nearestEnemy.IsSome then
             targetPos <- nearestEnemy.Value.Position
 
